2bd8c37bd08815978b3c5cb27c301f0d40b25a20
abkaaar <abdullahabubakar461@gmail.com>
2025-05-12 14:43:51 +0100

    fix: correct baseUrl assignment in const.ts for proper environment configuration

--- app/utils/const.ts
+++ app/utils/const.ts
@@ -1,4 +1,4 @@
-export const baseUrl = process.env.BASE_URL || "http://localhost:3000"
-// export const baseUrl = process.env.BASE_URL || "https://formlr.onrender.com"
+// export const baseUrl = process.env.BASE_URL || "http://localhost:3000"
+export const baseUrl = process.env.BASE_URL || "https://formlr.onrender.com"
 
 export const siteName = "Formlr - create surveys easily"
